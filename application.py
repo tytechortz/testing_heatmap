821114a696ab9dc9f9db81030d8422f3cb3999ce
James <tytechortz@gmail.com>
2022-11-25 09:12:45 -0700

    adds map and plots test data

--- application.py
+++ application.py
@@ -7,17 +7,37 @@ import pandas as pd
 import numpy as np
 from plotly.data import carshare
 from plotly.colors import sequential
+import datashader as ds
+
 
 # Mapbox token (replace with your own token string)
-mapbox_token = open(".mapbox_token").read()
-print(mapbox_token)
+mapbox_access_token = open(".mapbox_token").read()
+
+# print(mapbox_token)
 # Convert from lon/lat to web-mercator easting/northing coordinates
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/uber-rides-data1.csv')
+dff = df.query('Lat < 40.82').query('Lat > 40.70').query('Lon > -74.02').query('Lon < -73.91')
+
+
+cvs = ds.Canvas(plot_width=1000, plot_height=1000)
+agg = cvs.points(dff, x='Lon', y='Lat')
+# agg is an xarray object, see http://xarray.pydata.org/en/stable/ for more details
+coords_lat, coords_lon = agg.coords['Lat'].values, agg.coords['Lon'].values
+# Corners of the image, which need to be passed to mapbox
+coordinates = [[coords_lon[0], coords_lat[0]],
+               [coords_lon[-1], coords_lat[0]],
+               [coords_lon[-1], coords_lat[-1]],
+               [coords_lon[0], coords_lat[-1]]]
+
+from colorcet import fire
+import datashader.transfer_functions as tf
+img = tf.shade(agg, cmap=fire)[::-1].to_pil()
 df_original = carshare()
 df_original["easting"], df_original["northing"] = hv.Tiles.lon_lat_to_easting_northing(
 df_original["centroid_lon"], df_original["centroid_lat"]
 )
 
-# Duplicate carshare dataframe with Gaussian noise to produce a larger dataframe
+# # Duplicate carshare dataframe with Gaussian noise to produce a larger dataframe
 df = pd.concat([df_original] * 5000)
 df["easting"] = df["easting"] + np.random.randn(len(df)) * 400
 df["northing"] = df["northing"] + np.random.randn(len(df)) * 400
@@ -25,7 +45,7 @@ print(df)
 # Build Dataset and graphical elements
 dataset = hv.Dataset(df)
 points = hv.Points(df, ["easting", "northing"]).opts(color="crimson")
-tiles = hv.Tiles().opts(mapboxstyle="open-street-map", accesstoken=mapbox_token)
+tiles = hv.Tiles().opts(mapboxstyle="light", accesstoken=mapbox_access_token)
 overlay = tiles * datashade(points, cmap=sequential.Plasma)
 overlay.opts(
     title="Mapbox Datashader with %d points" % len(df),
@@ -36,6 +56,7 @@ overlay.opts(
 # Build App
 app = dash.Dash(__name__)
 components = to_dash(app, [overlay], reset_button=True)
+# components = to_dash(app, reset_button=True)
 
 app.layout = html.Div(
 components.children
